8ec8b6db6924f21120c696b4cbe55bbdeb413bf6
TaofikIlham <taofikilham3@gmail.com>
2023-02-17 19:11:28 +0700

    add contact

--- main.js
+++ main.js
@@ -11,3 +11,23 @@ function toggleDetail(e) {
     const detail = $(item).children(".about-exp-item-detail")
     $(detail).slideToggle()
 }
+
+function onFormSubmit(e) {
+    e.preventDefault()
+    const email = $("#inp_mail")
+    const subject = $("#inp_subject")
+    const message = $("#inp_message")
+
+    if(!$(email).val()) {
+        alert("Email Is Required")
+    } else if (!$(subject).val()) {
+        alert("Subject Is Required")
+    } else if (!$(message).val()) {
+        alert("Message Is Required")
+    } else {
+        alert("Form Submitted")
+        $(email).val("")
+        $(subject).val("")
+        $(message).val("")
+    }
+}
